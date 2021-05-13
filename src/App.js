12bf026d1cff510fff888019f8ea1e41effefe3b
Fernando Lobos <fernando.lobos@latam.com>
2021-05-12 22:45:50 -0400

    Movie Details added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutView from "./components/AboutView";
 import SearchView from "./components/SearchView";
 import {Switch, Route } from 'react-router-dom';
 import {useState, useEffect} from 'react';
+import MovieView from "./components/MovieView";
 
 function App() {
     const [searchResults, setSearchResults] = useState([]);
@@ -16,14 +17,14 @@ function App() {
 
 
     useEffect(() => {
-
+        if(searchText){
             fetch(`https://api.themoviedb.org/3/search/movie?api_key=a7880a8a80c677f979436aa96855b158&language=en-US&query=${searchText}&page=1&include_adult=false`)
                 .then(response => response.json())
                 .then(data => {
 
                     setSearchResults(data.results)
                 })
-
+        }
     }, [searchText])
 
 
@@ -39,6 +40,8 @@ function App() {
           <Route path="/search" exact>
               <SearchView keyword={searchText} searchResults={searchResults}/>
           </Route>
+
+          <Route path="/movies/:id" component={MovieView} />
       </Switch>
     </div>
   );
